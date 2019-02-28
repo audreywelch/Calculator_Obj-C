64e91f55a155b0223972cc5d5e081cfaf9f6a186
Audrey Welch <audreywelch@Audreys-MacBook-Pro.local>
2019-02-28 15:52:49 -0600

    Storyboard setup completed

---
+++ Calculator_Obj-C/Calculator_Obj-C/View Controllers/ALWViewController.h
@@ -0,0 +1,13 @@
+//
+//  ALWViewController.h
+//  
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ALWViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Calculator_Obj-C/Calculator_Obj-C/View Controllers/ALWViewController.m
@@ -0,0 +1,61 @@
+//
+//  ALWViewController.m
+//  
+
+#import "ALWViewController.h"
+
+@interface ALWViewController ()
+    
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+    
+- (IBAction)numberButtonTapped:(id)sender;
+    
+- (IBAction)decimalButtonTapped:(id)sender;
+    
+- (IBAction)returnButtonTapped:(id)sender;
+    
+- (IBAction)divideButtonTapped:(id)sender;
+    
+- (IBAction)multiplyButtonTapped:(id)sender;
+    
+- (IBAction)subtractButtonTapped:(id)sender;
+    
+- (IBAction)addButtonTapped:(id)sender;
+    
+@end
+
+@implementation ALWViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)decimalButtonTapped:(id)sender {
+}
+    
+- (IBAction)returnButtonTapped:(id)sender {
+}
+    
+- (IBAction)divideButtonTapped:(id)sender {
+}
+    
+- (IBAction)multiplyButtonTapped:(id)sender {
+}
+    
+- (IBAction)subtractButtonTapped:(id)sender {
+}
+    
+- (IBAction)addButtonTapped:(id)sender {
+}
+    @end
