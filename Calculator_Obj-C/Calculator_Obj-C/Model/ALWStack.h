1f4061e1b105f829cbb41cc594bb3a4c90ceb5a5
Audrey Welch <audreywelch@Audreys-MacBook-Pro.local>
2019-02-28 17:53:59 -0600

    Stack file skeleton created

---
+++ Calculator_Obj-C/Calculator_Obj-C/Model/ALWStack.h
@@ -0,0 +1,23 @@
+//
+//  ALWStack.h
+//  
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ALWStack : NSObject
+
+// Push
+    
+    
+// Pop
+    
+    
+// Peek
+    
+    
+    
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Calculator_Obj-C/Calculator_Obj-C/Model/ALWStack.m
@@ -0,0 +1,19 @@
+//
+//  ALWStack.m
+//  
+
+#import "ALWStack.h"
+
+@interface ALWStack ()
+
+// Values will be used every time the methods are called
+@property NSMutableArray *values;
+    
+@end
+
+@implementation ALWStack
+    
+    
+
+
+@end
